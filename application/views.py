0167f34a4e9c0bef20c038566370541548cfb4b3
211028 <211028@std.hi-joho.ac.jp>
2024-07-29 14:15:48 +0900

    change

--- application/views.py
+++ application/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    request.session.flush()
+    return render(request, 'index.html')
+
+def login(request):
+    if request.method == 'GET':
+        return render(request, 'login/login.html')
+
+    if request.method == 'POST':
+        return render(request, 'login/loginresult.html')
